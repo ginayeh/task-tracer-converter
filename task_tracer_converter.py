9de4dde64079ab3b0795e5017cd880e202647c31
Gina Yeh <gyeh@mozilla.com>
2014-10-24 16:03:08 +0800

    Task Tracer converter V2.0, compatible with Task Tracer V2.0. (WIP)
    Usage: python task_tracer_converter.py
           [-l LOG_FOLDER]
           [-m MMAP_FOLDER]
           -sp B2G-objdir/dist/bin/libxul.so
           -np B2G/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.7-4.6/x86_64-linux/bin/nm

--- task_tracer_converter.py
+++ task_tracer_converter.py
@@ -8,6 +8,9 @@ import json
 from sets import Set
 import sqlite3
 from collections import namedtuple
+from os import listdir
+from os.path import isfile
+import subprocess
 
 
 tasks = {}
@@ -15,6 +18,10 @@ show_warnings = False
 processes = {}
 threads = {}
 
+
+class ReadError(Exception):
+  def __init__(self, error_msg):
+    self.msg = error_msg
 
 class ParseError(Exception):
   def __init__(self, error_msg):
@@ -65,8 +72,6 @@ class Task(BaseObject):
     self.end = 0
     self.latency = 0
     self.executionTime = 0
-    self._userCpuTime = 0
-    self._sysCpuTime = 0
 
   def add_label(self, timestamp, label):
     self.labels.append(Label(timestamp, label))
@@ -83,7 +88,8 @@ def find_char_and_split(string, char=' ', num_split=-1):
     raise ParseError('Extract error: no \'{}\''.format(char))
 
   if string.count(char) < num_split:
-    raise ParseError('Extract error: not enough \'{}\'s'.format(char))
+    print num_split, string
+    raise ParseError('Extract error: no enough \'{}\''.format(char))
 
 
   if num_split == -1:
@@ -91,100 +97,45 @@ def find_char_and_split(string, char=' ', num_split=-1):
   else:
     return string.split(char, num_split)
 
-def extract_info(log):
-  """
-    Discard redundant message and extract information.
-
-    log: Raw log.
-      Ex. I/TaskTracer( 1570): 1 6743098656705 316475166353175 6743098656704 1 6743098656704
-
-    returns:
-      A list is returned after successfully extract information from input.
-      None is returned if the input is redundant.
-      False is returned  when an error occurred.
-  """
-  raw_log = log
-  try:
-    # Remove tag name. Ex. 'I/TaskTracer( 1570):'
-    tokens = find_char_and_split(log, ':', 1)
-    log = tokens[1].strip()
-
-    # Retrieve log type
-    tokens = find_char_and_split(log, ' ', 1)
-    log_type = int(tokens[0])
-    log = tokens[1].strip()
-
-    # log_type:
-    #   0 - DISPATCH. Ex. 0 taskId dispatch sourceEventId sourceEventType parentTaskId
-    #   1 - BEGIN.    Ex. 1 taskId begin userCpuTime sysCpuTime processId "processName" threadId "threadName"
-    #   2 - END.      Ex. 2 taskId end userCpuTime sysCpuTime
-    #   3 - LABEL.    Ex. 3 taskId timestamp "label"
-    #   4 - VPTR.     Ex. 4 address
-    info = None
-
-    if log_type == 3:
-      tokens = find_char_and_split(log, ' ', 2)
-      info = [log_type] + tokens[0:2]
-
-      tokens = find_char_and_split(tokens[2], '"', 2)
-      info.append(tokens[1])
-    elif log_type == 1:
-      tokens = find_char_and_split(log, ' ', 5)
-      info = [log_type] + tokens[0:5]
-
-      tokens = find_char_and_split(tokens[5], '"', 4)
-      info.append(tokens[1])
-      info.append(tokens[2].strip())
-      info.append(tokens[3])
-    else:
-      tokens = find_char_and_split(log)
-      info = [log_type] + tokens
-  except ParseError:
-    raise
-
-  return info
-
 def verify_info(info):
   """
-    Verify information value and format.
+    Verify task information based on log type.
 
-    info: A list of information.
+    info: A list of task properties.
+      DISPATCH: [0 taskId dispatch sourceEventId sourceEventType parentTaskId]
+      BEGIN:    [1 taskId begin processId threadId]
+      END:      [2 taskId end]
+      LABEL:    [3 taskId timestamp, label]
+      VPTR:     [4 taskId vptr]
 
     returns:
-      True when verify passed.
-      False when verify failed.
+      True when verification passed.
+      False when verification failed.
   """
-  log_type = int(info[0])
+  log_type = info[0]
   if not log_type in range(0, 5):
     raise ParseError('Verify error: invalid log type \'{}\''.format(log_type))
 
   if any(((log_type == 0) and (len(info) != 6),
-          (log_type == 1) and (len(info) != 9),
-          (log_type == 2) and (len(info) != 5),
-          (log_type == 3) and (len(info) != 4))):
+          (log_type == 1) and (len(info) != 5),
+          (log_type == 2) and (len(info) != 3),
+          (log_type == 3) and (len(info) != 4),
+          (log_type == 4) and (len(info) != 3))):
      raise ParseError('Verify error: incomplete information')
 
-def set_task_info(info):
+def set_task_info(info, process_id):
   """
-    Set task properties based on log_type.
+    Set task properties based on log type.
 
-    info: A list of information. Each type has its own format.
-      DISPATCH: [0 taskId dispatch sourceEventId sourceEventType parentTaskId]
-      BEGIN:    [1 taskId begin userCpuTime sysCpuTime processId processName threadId threadName]
-      END:      [2 taskId end userCpuTime sysCpuTime]
-      LABEL:    [3 taskId timestamp, label]
-      VPTR:     [4 taskId vptr]
+    info: A list of verified task properties.
+    process_id: Default value of Task.processId.
   """
-  log_type = int(info[0])
-
+  log_type = info[0]
   task_id = info[1]
   if task_id not in tasks:
-    if log_type == 0:
-      tasks[task_id] = Task(int(task_id))
-    else:
-      if show_warnings:
-        print 'Skip task {} because of incomplete logs.'.format(task_id)
-      return
+    tasks[task_id] = Task(int(task_id))
+    tasks[task_id].processId = int(process_id)
+    tasks[task_id].processName = processes[process_id].name
 
   if log_type == 4:
     tasks[task_id]._vptr = int(info[2], 16)
@@ -199,66 +150,49 @@ def set_task_info(info):
   elif log_type == 1:
     tasks[task_id].begin = timestamp
     tasks[task_id].latency = tasks[task_id].begin - tasks[task_id].dispatch
-    tasks[task_id]._userCpuTime = int(info[3])
-    tasks[task_id]._sysCpuTime = int(info[4])
 
-    process_id = int(info[5])
-    tasks[task_id].processId = process_id
-    if process_id not in processes:
-      processes[process_id] = Process(process_id, info[6])
-    elif all((info[6] != processes[process_id].name,
-              info[6] != '(Preallocated app)',
-              info[6] != '')):
-      processes[process_id].name = info[6]
-
-    thread_id = int(info[7])
+    thread_id = int(info[4])
+    # For threads which aren't registered, they may have no name.
+    if info[4] not in threads:
+      threads[info[4]] = Thread(thread_id, '')
     tasks[task_id].threadId = thread_id
-    if thread_id not in threads:
-      threads[thread_id] = Thread(thread_id, info[8])
-    else:
-      threads[thread_id].name = info[8]
+    tasks[task_id].threadName = threads[info[4]].name
   elif log_type == 2:
     tasks[task_id].end = timestamp
     tasks[task_id].executionTime = tasks[task_id].end - tasks[task_id].begin
-    tasks[task_id]._userCpuTime -= int(info[3])
-    tasks[task_id]._sysCpuTime -= int(info[4])
-
-    if tasks[task_id]._userCpuTime != 0:
-      cpuTime = -tasks[task_id]._userCpuTime / float(tasks[task_id].executionTime) * 100.0
-      tasks[task_id].add_label(timestamp, 'UserCpuTime:{}'.format(round(cpuTime, 2)))
-
-    if tasks[task_id]._sysCpuTime != 0:
-      cpuTime = -tasks[task_id]._sysCpuTime / float(tasks[task_id].executionTime) * 100.0
-      tasks[task_id].add_label(timestamp, 'SysCpuTime: {}'.format(round(cpuTime, 2)))
   elif log_type == 3:
     tasks[task_id].add_label(timestamp, info[3])
 
-def parse_log(input_name):
+def parse_log(log, process_id):
   """
-    Parse input file line by line.
-
-    input_name: Input filename.
-
-    returns:
-      True if the whole file is parsed successfully.
-      False when error occurred.
+    Parse log line by line and verify the parsing results based on the log type.
+    Then, set up task information with the verified parsing results.
   """
-  with open(input_name, 'r') as log_file:
-    all_log = log_file.readlines()
-
-  for line in all_log:
-    # Remove dummy log like '----------- beginning of...'
-    if line.startswith('-'):
-      continue
-
+  for line in log:
+    info = None
     try:
-      info = extract_info(line.strip())
+      # Get log type
+      [log_type, remain] = find_char_and_split(line.strip(), ' ', 1)
+
+      # log_type:
+      #   0 - DISPATCH. Ex. 0 taskId dispatch sourceEventId sourceEventType parentTaskId
+      #   1 - BEGIN.    Ex. 1 taskId begin processId threadId
+      #   2 - END.      Ex. 2 taskId end
+      #   3 - LABEL.    Ex. 3 taskId timestamp "label"
+      #   4 - VPTR.     Ex. 4 address
+      if log_type == '3':
+        [task_id, timestamp, remain] = find_char_and_split(remain, ' ', 2)
+        info = [int(log_type), task_id, timestamp, remain.replace("\"", "")]
+      else:
+        tokens = find_char_and_split(remain)
+        info = [int(log_type)] + tokens
+    
       verify_info(info)
     except ParseError as error:
       error.log = line.strip()
       raise
 
-    set_task_info(info)
+    set_task_info(info, process_id)
 
 def retrieve_begin_end_time():
   """Scan through all timestamps and return the min and the max."""
@@ -273,14 +207,6 @@ def retrieve_begin_end_time():
     all_timestamps.remove(0)
 
   return [min(all_timestamps), max(all_timestamps)]
-
-def replace_undefined_timestamp(end_time):
-  """Replace undefined timestamp with the max of all timestamps."""
-  for task_id, task_object in tasks.iteritems():
-    if task_object.begin == 0:
-      task_object.begin = end_time
-    if task_object.end == 0:
-      task_object.end = end_time
 
 def output_json(output_name, begin_time, end_time):
   """
@@ -308,9 +234,10 @@ def output_json(output_name, begin_time, end_time):
 def get_arguments(argv):
   """Parse arguments."""
   parser = argparse.ArgumentParser()
-  parser.add_argument('-i', '--input-file', help='Input file', required=True)
-  parser.add_argument('-o', '--output-file', help='Output file (Optional)',
-                      default='task_tracer_data.json')
+  parser.add_argument('-l', '--input-log-folder', help='Input log folder', default='log')
+  parser.add_argument('-m', '--input-mmap-folder', help='Input mmap folder', default='mmap')
+  parser.add_argument('-sp', '--symbol-path', help='libxul.so path', required=True)
+  parser.add_argument('-np', '--nm-path', help='nm path', required=True)
   parser.add_argument('-c', '--check-parent-task-id', action='store_const',
                       const=True, help='Check parentTaskId (if possible)')
   parser.add_argument('-w', '--show-warnings', action='store_const', const=True,
@@ -329,18 +256,13 @@ def create_table_and_insert_tasks():
     # Delete the table if exists and re-create the table
     cur.execute('DROP TABLE IF EXISTS Tasks')
     cur.execute(('CREATE TABLE Tasks('
-                 'taskId INT, threadId INT, begin INT, end INT)'))
+                 'taskId INT, threadId INT, begin INT, end INT, name TEXT, sourceEventId INT)'))
 
     # Insert information into table
     for task_id, task_obj in tasks.iteritems():
-      # Only tasks which includes complete information are inserted into database
-      if any((task_obj.threadId is None,
-              task_obj.begin is 0,
-              task_obj.end is 0)):
-        continue
-
-      insert_cmd = ('INSERT INTO Tasks VALUES({}, {}, {}, {})'.format(task_id,
-                    task_obj.threadId, task_obj.begin, task_obj.end))
+      insert_cmd = ('INSERT INTO Tasks VALUES({}, {}, {}, {}, \'{}\', {})'.format(task_id,
+                    task_obj.threadId, task_obj.begin, task_obj.end,
+                    task_obj.name, task_obj.sourceEventId))
       cur.execute(insert_cmd)
 
     cur.execute('SELECT * FROM Tasks')
@@ -419,7 +341,7 @@ def print_all_tasks():
            'sourceEventType: {}, '.format(task_obj.sourceEventType) +
            'sourceEventId: {}, '.format(task_obj.sourceEventId) +
            'processId: {}, '.format(task_obj.processId) +
-           'threadID: {}, '.format(task_obj.threadId) +
+           'threadId: {}, '.format(task_obj.threadId) +
            'parentTaskId: {}, '.format(task_obj.parentTaskId) +
            'dispatch: {}, '.format(task_obj.dispatch) +
            'begin: {}, '.format(task_obj.begin) +
@@ -432,82 +354,130 @@ def binary_search(address, x, lo=0, hi=None):
     while lo < hi:
       if (hi - lo == 1):
         return address[lo][1]
-
       mid = (lo + hi) / 2
       midval = address[mid][0]
       if (midval < x):
-        lo = mid + 1
+        lo = mid
       elif (midval > x):
         hi = mid
       else:
-        return mid
+        return address[mid][1]
 
-def retrieve_task_name():
-  """
-    
-  """
-  # Read file 'mem_offset' generated by prepare-data.sh
-  with open('mem_offset', 'r') as mmaps_file:
-    all_mem_offset = mmaps_file.readlines()
-
-  for line in all_mem_offset:
-    try:
-      tokens = find_char_and_split(line)
-    except ParseError:
-      raise
-
-    process_id = int(tokens[0].strip())
-    if process_id not in processes:
-      continue
-
-    processes[process_id]._mem_offset = int(tokens[1].strip(), 16)
-
-  # Read file 'symbol' generated by prepare-data.sh
-  with open('symbol', 'r') as symbol_file:
-    all_symbols = symbol_file.readlines()
+def retrieve_task_name(nm_path, symbol_path):
+  """Retrieve symbol from libxul.so"""
+  p1 = subprocess.Popen([nm_path, '-a', symbol_path], stdout=subprocess.PIPE)
+  p2 = subprocess.Popen(['grep', '_Z'], stdin=p1.stdout, stdout=subprocess.PIPE)
+  p3 = subprocess.Popen(['c++filt'], stdin=p2.stdout, stdout=subprocess.PIPE)
+  p4 = subprocess.Popen(['sort'], stdin=p3.stdout, stdout=subprocess.PIPE)
+  p1.stdout.close()
+  p2.stdout.close()
+  p3.stdout.close()
+  output = p4.communicate()[0]
+  all_symbols = find_char_and_split(output, '\n')
 
   address = []
   for line in all_symbols:
+    if len(line) == 0:
+      continue
+
     try:
-      tokens = find_char_and_split(line, ' ', 4)
+      tokens = find_char_and_split(line, ' ', 2)
     except ParseError:
       raise
 
     if len(tokens[0]) == 0:
       continue
 
-    address.append((int(tokens[0], 16), tokens[4].strip()))
+    address.append((int(tokens[0], 16), tokens[2].strip()))
 
   # Get name for each task
   for task_id, task_obj in tasks.iteritems():
     if not (task_obj._vptr and task_obj.processId and
-      processes[task_obj.processId]._mem_offset):
+      processes[str(task_obj.processId)]._mem_offset):
       if show_warnings:
         print 'Skip task {} because of incomplete mem_offset.'.format(task_id)
       continue
 
-    offset = task_obj._vptr - processes[task_obj.processId]._mem_offset
+    offset = task_obj._vptr - processes[str(task_obj.processId)]._mem_offset
 
     task_obj.name = binary_search(address, offset)
+
+    if not task_obj.name:
+      print task_obj._vptr, processes[str(task_obj.processId)]._mem_offset, offset
+
+def read_log(input_folder):
+  """
+    Iterate through input folder and read all log files. Process information are
+    set according to the filename. Task information and thread information are
+    included in the json file.
+  """
+  for filename in listdir(input_folder):
+    if not filename.startswith('profile') or not isfile(input_folder + '/' + filename):
+      raise ReadError('Unrecognized log file: ' + filename)
+
+    # Set up process info. Example filename: profile_3810_b2g.txt
+    [name, ext] = find_char_and_split(filename, '.', 1)
+    [prefix, process_id, process_name] = find_char_and_split(name, "_")
+    processes[process_id] = Process(int(process_id), process_name)
+
+    # Load json file and get tasktracer log.
+    with open(input_folder + '/' + filename, 'r') as json_file:
+      json_data = json.load(json_file)
+      task_info = json_data["tasktracer"]["data"]
+      thread_info = json_data["tasktracer"]["threads"]
+
+    # Set up thread info.
+    for t in thread_info:
+      thread_id = t["tid"]
+      thread_name = t["name"]
+      # FIXME
+      if thread_name == 'GeckoMain':
+        thread_id = int(process_id)
+      threads[thread_id] = Thread(thread_id, thread_name)
+
+    parse_log(task_info, process_id)
+
+def read_mmap(input_folder):
+  for filename in listdir(input_folder):
+    [prefix, process_id] = find_char_and_split(filename, '_')
+    if str(process_id) not in processes:
+      continue
+  
+    with open(input_folder + '/' + filename, 'r') as mmap_file:
+      mmap_data = mmap_file.readlines()
+
+    for line in mmap_data:
+      if 'libxul.so' in line:
+        [mem_offset, others] = find_char_and_split(line, '-', 1);
+        processes[str(process_id)]._mem_offset = int(mem_offset, 16)
+        break
 
 def main(argv=sys.argv[:]):
   args = get_arguments(argv)
 
-  print 'Input:', args.input_file
-  print 'Output:', args.output_file
+  print 'Input log folder:', args.input_log_folder
+  print 'Input mmap folder:', args.input_mmap_folder
+  print 'symbol path: ', args.symbol_path
+  print 'nm path: ', args.nm_path
+  print 'Output: task_tracer_data.json'
 
   global show_warnings
   show_warnings = args.show_warnings
 
+  log = []
   try:
-    parse_log(args.input_file)
+    read_log(args.input_log_folder)
     print len(tasks), 'tasks has been created successfully.'
 
-    retrieve_task_name()
+    read_mmap(args.input_mmap_folder)
+    retrieve_task_name(args.nm_path, args.symbol_path)
   except ParseError as error:
     print error.msg
     if error.log:
       print '@line: \'{}\''.format(error.log)
+    sys.exit()
+  except ReadError as error:
+    print error.msg
     sys.exit()
 
   if args.check_parent_task_id:
@@ -517,9 +487,8 @@ def main(argv=sys.argv[:]):
     sys.exit()
 
   [begin_time, end_time] = retrieve_begin_end_time();
-  replace_undefined_timestamp(end_time);
 
-  output_json(args.output_file, begin_time, end_time)
+  output_json('task_tracer_data.json', begin_time, end_time)
 
   print len(tasks), 'tasks has been written to JSON output successfully.'
   if args.print_all_tasks:
